8b352fc158c414d74e731bdc09965386e8876217
Andrew Yang <andy.tubeee@gmail.com>
2020-10-14 13:42:25 -0400

    Refactored to prevent freezing

--- TrigClock.py
+++ TrigClock.py
@@ -1,4 +1,3 @@
-from time import sleep
 from tkinter import *
 from datetime import datetime
 import math
@@ -40,44 +39,49 @@ for i in range(60):
         continue
     screen.create_oval(x, y, x + 6, y + 6, fill="white")
 
-while True:
-    # Time info
-    hour = datetime.now().hour
-    minute = datetime.now().minute
-    second = datetime.now().second
 
-    # Tkinter angle correction
-    angle_correction = -90
+def updateClock():
+    while True:
+        # Time info
+        hour = datetime.now().hour
+        minute = datetime.now().minute
+        second = datetime.now().second
 
-    # Angle information
-    hour_angle = (((hour % 12) + (minute / 60)) / 12) * 360 + angle_correction
-    minute_angle = ((minute + (second / 60)) / 60) * 360 + angle_correction
-    second_angle = ((second / 60) * 360) + angle_correction
+        # Tkinter angle correction
+        angle_correction = -90
 
-    # Update second
-    angle_in_radians = second_angle * math.pi / 180
-    hand_length = clock_radius - 40
-    end_x = ORIGIN[0] + hand_length * math.cos(angle_in_radians)
-    end_y = ORIGIN[1] + hand_length * math.sin(angle_in_radians)
-    secondHand = screen.create_line(ORIGIN, end_x, end_y, width=8, fill="blue")
+        # Angle information
+        hour_angle = (((hour % 12) + (minute / 60)) / 12) * 360 + angle_correction
+        minute_angle = ((minute + (second / 60)) / 60) * 360 + angle_correction
+        second_angle = ((second / 60) * 360) + angle_correction
 
-    # Update minute
-    angle_in_radians = minute_angle * math.pi / 180
-    hand_length = clock_radius - 70
-    end_x = ORIGIN[0] + hand_length * math.cos(angle_in_radians)
-    end_y = ORIGIN[1] + hand_length * math.sin(angle_in_radians)
-    minuteHand = screen.create_line(ORIGIN, end_x, end_y, width=13, fill="red")
+        # Update second
+        angle_in_radians = second_angle * math.pi / 180
+        hand_length = clock_radius - 40
+        end_x = ORIGIN[0] + hand_length * math.cos(angle_in_radians)
+        end_y = ORIGIN[1] + hand_length * math.sin(angle_in_radians)
+        secondHand = screen.create_line(ORIGIN, end_x, end_y, width=8, fill="blue")
 
-    # Update hour
-    angle_in_radians = hour_angle * math.pi / 180
-    hand_length = clock_radius - 110
-    end_x = ORIGIN[0] + hand_length * math.cos(angle_in_radians)
-    end_y = ORIGIN[1] + hand_length * math.sin(angle_in_radians)
-    hourHand = screen.create_line(ORIGIN, end_x, end_y, width=19, fill="green")
+        # Update minute
+        angle_in_radians = minute_angle * math.pi / 180
+        hand_length = clock_radius - 70
+        end_x = ORIGIN[0] + hand_length * math.cos(angle_in_radians)
+        end_y = ORIGIN[1] + hand_length * math.sin(angle_in_radians)
+        minuteHand = screen.create_line(ORIGIN, end_x, end_y, width=13, fill="red")
 
-    # Animation
-    screen.update()
-    sleep(1)
-    screen.delete(secondHand, minuteHand, hourHand)
+        # Update hour
+        angle_in_radians = hour_angle * math.pi / 180
+        hand_length = clock_radius - 110
+        end_x = ORIGIN[0] + hand_length * math.cos(angle_in_radians)
+        end_y = ORIGIN[1] + hand_length * math.sin(angle_in_radians)
+        hourHand = screen.create_line(ORIGIN, end_x, end_y, width=19, fill="green")
+
+        # Animation
+        screen.update()
+        screen.delete(secondHand, minuteHand, hourHand)
+
+
+# Prevent while True loop from freezing
+screen.after(1, updateClock())
 
 screen.mainloop()
