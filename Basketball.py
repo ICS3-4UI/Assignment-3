146e69266e94edf4eb82966ee38192ba4424e0ae
Andrew Yang <andy.tubeee@gmail.com>
2020-10-14 16:07:10 -0400

    Update asphalt

--- Basketball.py
+++ Basketball.py
@@ -24,6 +24,16 @@ def create_circle(x, y, r, screenName, color):
 top_right, bottom_left = [0, 525], [WIDTH + 1, HEIGHT + 1]
 screen.create_rectangle(top_right, bottom_left, fill="grey", outline="")
 
+colour = ["black", "silver", "grey", "slategray"]
+# Asphalt
+for i in range(1100):
+    x = rd.randint(0, WIDTH)
+    y = rd.randint(525, HEIGHT)
+    s = rd.randint(1, 4)
+
+    c = rd.choice(colour)
+    a = screen.create_oval(x, y, x + s, y + s, fill=c, outline="")
+
 # Sun
 create_circle(900, 100, 80, screen, "gold")
 
@@ -55,20 +65,6 @@ screen.create_rectangle(685, 385, 724, 386, width=3, outline="brown")
 
 n_tl, n_br = [725, 300], [735, 675]
 screen.create_rectangle(n_tl, n_br, fill="white", outline="")
-
-colour = ["black", "silver", "grey", "slategray"]
-# Asphalt
-for i in range(1100):
-    x = rd.randint(0, WIDTH)
-    y = rd.randint(525, HEIGHT)
-    s = rd.randint(1, 4)
-
-    # Prevent asphalt from spawning on net bar
-    if n_tl[0] <= x + s <= n_br[0] and n_tl[1] <= y + s <= n_br[1]:
-        continue
-
-    c = rd.choice(colour)
-    a = screen.create_oval(x, y, x + s, y + s, fill=c, outline="")
 
 # Strings
 screen.create_line(610, 381, 620, 450, fill="white", width=3)
